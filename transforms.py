a976db152b081bb8a2e2abce25d125ccb11773cb
windygooo <127473552+windygooo@users.noreply.github.com>
2023-03-22 14:17:14 +0800

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -8,30 +8,22 @@ class Preprocessing(object):
                  means=(0.485, 0.456, 0.406),
                  stds=(0.229, 0.224, 0.225),
                  augmentor=None):
+        self.trans = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize(means, stds)])
         self.augmentor = augmentor
-
-        self.trans = transforms.Compose([transforms.ToTensor()])
-        self.means = torch.tensor(means)
-        self.stds = torch.tensor(stds)
 
     def __call__(self, sample: dict):
         if self.augmentor is not None:
             sample = self.augmentor(**sample)
+            del sample['transform_params']
 
-        data = list(sample.values())
-        labels = torch.tensor([], dtype=torch.long)
+        res = list(sample.values())
+        labels = []
+        img = self.trans(res[0])
 
-        for i in range(len(data)):
-            if not len(data[i]):
-                data[i] = torch.zeros(1, 0, 2, dtype=torch.int)
-            else:
-                data[i] = self.trans(np.array(data[i]))
+        for i in range(1, len(res)):
+            res[i] = torch.tensor(res[i])
+            labels.append(torch.full((len(res[i]),), i - 1))
+            res[i] = res[i].reshape(-1)
 
-            if i == 0:  # image standardization
-                for t, m, s in zip(data[0], self.means, self.stds):
-                    t.sub_(m).div_(s)
-            else:  # reshape to 2N-length vector for padding
-                labels = torch.cat([labels, torch.full((data[i].shape[1],), i - 1)])
-                data[i] = data[i].reshape(-1)
-
-        return data[0], torch.cat(data[1:]), labels
+        return img, torch.cat(res[1:]), torch.cat(labels)
